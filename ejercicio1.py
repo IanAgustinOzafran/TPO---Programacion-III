f0074f7187b194ce3539b63610110aa555074f5e
IAN <128194052+IanAgustinOzafran@users.noreply.github.com>
2025-10-24 19:34:20 -0300

    Update ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,8 @@
+''' 
+Parte 1: Divide & Conquista
+Problema: Par de puntos mas cercano
+
+En un plano bidimensional se tienen n puntos con coordenadas (x, y). El objetivo es 
+encontrar el par de puntos que estén a la menor distancia posible entre sí. 
+
+'''
